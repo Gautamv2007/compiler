4ba9536a439d748602d7cbca9e14e08eacdc35cf
Gautamv2007 <gautam.rrv@gmail.com>
2026-03-07 02:50:48 +0530

    Adding lexer and little bit of parser

---
+++ src/include/AST.h
@@ -0,0 +1,18 @@
+#ifndef TAC_AST_H
+#define TAC_AST_H
+typedef struct AST_STRUCT
+{
+  enum 
+  {
+    AST_COMPOUND,
+    AST_FUNCTION_DEFINITION,
+    AST_DEFINITION_TYPE,
+    AST_VARIABLE,
+    AST_STATEMENT,
+    AST_NOOP,
+  } type;
+  
+} AST_T;
+
+AST_T* init_ast(int type);
+#endif
