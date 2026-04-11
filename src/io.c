8c6e49e059c0d1ceee1f4465c4fa13843647634d
Gautamv2007 <gautam.rrv@gmail.com>
2026-04-11 20:20:37 +0530

    Making only files with extension .gv or .txt to be accepted

--- src/io.c
+++ src/io.c
@@ -4,6 +4,13 @@
 #include <string.h>
 
 char* tac_read_file(const char* filename) {
+    const char* ext = strrchr(filename, '.');
+
+    if (!ext || (strcmp(ext, ".gv") != 0 && strcmp(ext, ".txt") != 0)) {
+        printf("[Error]: Invalid file format '%s'. Only .gv or .txt files are allowed for compilation.\n", filename);
+        exit(1);
+    }
+
     FILE* fp = fopen(filename, "rb");
     if (!fp) {
         printf("Could not open file %s\n", filename);
